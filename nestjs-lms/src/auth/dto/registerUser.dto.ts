1d75dc1cc07add44fd44b460bea479fd83b9d865
Subham Sahu <about.subhamsahu@gmail.com>
2025-10-28 22:02:07 +0530

    registerUser.dto.ts - updated the RegisterUserDto class with class-validator & swagger

--- nestjs-lms/src/auth/dto/registerUser.dto.ts
+++ nestjs-lms/src/auth/dto/registerUser.dto.ts
@@ -1,6 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
 export class RegisterUserDto {
+  @ApiProperty({ example: 'John' })
+  @IsString()
   fname: string;
+
+  @ApiProperty({ example: 'Doe' })
+  @IsString()
   lname: string;
+
+  @ApiProperty({ example: 'john@example.com' })
+  @IsEmail()
   email: string;
+
+  @ApiProperty({ example: 'StrongP@ss123' })
+  @IsString()
+  @MinLength(8)
   password: string;
 }
