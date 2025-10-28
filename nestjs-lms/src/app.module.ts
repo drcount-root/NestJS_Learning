72c468a3f8b42d7a97b4ee2d1b9d005df3ab9026
Subham Sahu <about.subhamsahu@gmail.com>
2025-10-28 22:35:36 +0530

    env added

--- nestjs-lms/src/app.module.ts
+++ nestjs-lms/src/app.module.ts
@@ -3,12 +3,14 @@ import { HealthModule } from './health/health.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     HealthModule,
     AuthModule,
     UserModule,
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_URL as string),
   ],
   controllers: [],
